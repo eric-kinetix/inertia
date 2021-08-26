4fb31ce99b5aaf22a3fd210f9b1a864dcefa2fbd
Eric Martinez <eric@kinetix.energy>
2021-08-26 16:24:58 -0700

    Removed log from archive data

--- build/run_every_5_min.sh
+++ build/run_every_5_min.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/python3 /home/moxa/build/current/archive_data.py >> /tmp/archive_out_log
+/usr/bin/python3 /home/moxa/build/current/archive_data.py
 sshpass -p $HOSTNAME /usr/bin/rsync -zuv /home/moxa/data/archive/* $HOSTNAME@50.116.12.234:/home/$HOSTNAME/data/archive >> /tmp/rsync_out_log
